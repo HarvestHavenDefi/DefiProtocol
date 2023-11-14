da01bb8e9ef499e46d0d516a50d1c0bfd75f4ce4
CristianRicharte6 <cristianricharte6@gmail.com>
2023-11-14 12:30:55 +0100

    init v1

---
+++ contracts/Harvest.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @title HARVEST ERC-20
+/// @author Cristian Richarte Gil - Harvest
+
+contract Harvest is ERC20 {
+    constructor(
+        address manager,
+        uint256 amount
+    ) payable ERC20("Harvest", "HVR") {
+        _mint(manager, amount * 10 ** decimals());
+    }
+}
---
+++ contracts/stHarvest.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Harvest.sol";
+
+/// @title HARVEST ERC-20
+/// @author Cristian Richarte Gil - Harvest
+/// @notice ERC-20 token
+/// @dev Explain to a developer any extra details
+contract Harvest is ERC20 {
+    // Mapping to track the allowed minters. It´s meant to be DefiContracts whitelisted.
+    mapping(address => bool) public isMinter;
+
+    modifier onlyMinter() {
+        require(isMinter[msg.sender], "Not allowed");
+        _;
+    }
+
+    constructor()
+        payable
+        ERC20("staked Harvest", "stHVR")
+        Ownable(msg.sender)
+    {}
+
+    function mint(uint256 amount, address receiver) external onlyMinter {
+        _mint(receiver, amount);
+    }
+
+    function updateMinter() external onlyOwner {}
+
+    function burn(uint256 amount) external onlyOwner {
+        _burn(msg.sender, amount);
+    }
+}
