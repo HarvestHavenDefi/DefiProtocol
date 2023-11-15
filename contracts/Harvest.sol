89fd3b0b042ae535e2f3f4811a48eb39f49c3019
CristianRicharte6 <cristianricharte6@gmail.com>
2023-11-15 09:28:37 +0100

    Style: Title update

--- contracts/Harvest.sol
+++ contracts/Harvest.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.23;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/// @title HARVEST ERC-20
+/// @title HARVEST ERC-20 (HVR)
 /// @author Cristian Richarte Gil - Harvest Heaven
 
 contract Harvest is ERC20 {
